a66f6d5c6ead3fef62453a70a9425853ff694675
Andrew Garr <chico2995@gmail.com>
2020-08-17 03:18:38 +0800

    Add Joomla files
    Added Joomla Files

---
+++ joomla/administrator/logs/error.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2020-07-31 11:21:18 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2020-07-31T11:21:18+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-31T11:21:27+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-31T11:21:32+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-31T11:21:37+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-31T11:21:48+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-07-31T12:41:29+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-12T13:13:10+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-12T13:13:13+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-12T13:13:17+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-12T13:13:23+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
+2020-08-12T13:13:32+00:00	INFO 116.88.113.227	joomlafailure	Username and password do not match or you do not have an account yet.
